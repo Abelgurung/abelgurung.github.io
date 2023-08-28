3dc04cf7b7e574b2e0e8139f1a45e4ccb070a8f7
Abel Gurung <abelgurung@Abels-MacBook-Pro-2.local>
2023-08-28 00:54:08 -0500

    safari img bug

--- src/components/Main-page/Socials.js
+++ src/components/Main-page/Socials.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className=" gap-4 md:flex md:flex-col grid grid-cols-12 items-center py-4 md:p-1 ">
       <img src="headshot.png" className="h-full md:h-[15rem] col-span-4 " />
-      <div className="space-y-3 pt-1 col-span-8">
+      <div className="space-y-3 pt-1 col-span-8 ">
         <h1 className="font-bold ">Abel Gurung</h1>
         <a href="https://www.linkedin.com/in/abel-gurung-27a478226/">
           <i class="fab fa-linkedin"></i> Linkedin
