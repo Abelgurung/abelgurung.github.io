3d07279cb643c5ef38e601779871ba92b9e79b28
Abel Gurung <abelgurung.ree@gmail.com>
2023-11-10 20:08:28 -0600

    intro changes

--- src/components/Main-page/Intro.js
+++ src/components/Main-page/Intro.js
@@ -1,4 +1,21 @@
-const intro =
-  " I'm currently in my final year of undergrad, majoring in Mathematics with a minor in Computer Science. I am broadly interested in leveraging computational methods to develop tools and simulations that accelerate advancements in biomedical research. My background includes hands-on research experience in the fields of biomedical research, computational biology, and machine learning. ";
+// const intro =
+//   " I'm in my final year of undergrad, majoring in Mathematics with a minor in Computer Science and Biological Sciences. I am broadly interested in leveraging computational methods to develop tools and simulations that accelerate advancements in biomedical research. My background includes hands-on research experience in the fields of biomedical research, computational biology, and machine learning. ";
+
+// export default intro;
+// In your CSS file (e.g., App.css)
+
+// In your React component file
+const intro = (
+  <span>
+    I'm in my final year of undergrad, majoring in{" "}
+    <span className="font-medium  ">Mathematics</span> with a minor in{" "}
+    <span className="font-medium ">Computer Science</span> and{" "}
+    <span className="font-medium ">Biological Sciences</span>. I am broadly
+    interested in leveraging computational methods to develop tools and
+    simulations that accelerate advancements in biomedical research. My
+    background includes hands-on research experience in biomedical research,
+    computational biology, and machine learning.
+  </span>
+);
 
 export default intro;
